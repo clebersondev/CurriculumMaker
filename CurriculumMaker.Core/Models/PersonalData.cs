4e0985760f7e777e8698d204f2b1c5368d5cbadf
Cleberson Pinheiro <clebersonpinheiro.net@gmail.com>
2026-05-13 09:10:39 -0300

    refactor: update PersonalData model properties and constructor

--- CurriculumMaker.Core/Models/PersonalData.cs
+++ CurriculumMaker.Core/Models/PersonalData.cs
@@ -1,15 +1,38 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace CurriculumMaker.Core.Models
+namespace CurriculumMaker.Core.Models;
+
+public sealed class PersonalData
 {
-    public class PersonalData
+    public PersonalData() { }
+
+    public PersonalData(
+        string name,
+        string surname,
+        string email,
+        string phoneNumber,
+        string address,
+        string city,
+        string state)
     {
-        [Required] public string Name { get; set; } = string.Empty;
-        [Required] public string Surname { get; set; } = string.Empty;
-        [EmailAddress] public string Email { get; set; } = string.Empty;
-        [Required] public string PhoneNumber { get; set; } = string.Empty;
-        [Required] public string Address { get; set; } = string.Empty;
-        [Required] public string City { get; set; } = string.Empty;
-        [Required] public string Country { get; set; } = string.Empty;
+        Name = name;
+        Surname = surname;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        City = city;
+        State = state;
     }
+
+    public int Id { get; set; }
+    [Required] public string Name { get; set; } = string.Empty;
+    [Required] public string Surname { get; set; } = string.Empty;
+    [EmailAddress] public string Email { get; set; } = string.Empty;
+    [Required] public string PhoneNumber { get; set; } = string.Empty;
+    [Required] public string Address { get; set; } = string.Empty;
+    [Required] public string City { get; set; } = string.Empty;
+    [Required] public string State { get; set; } = string.Empty;
+
+    public string FullName => $"{Name} {Surname}";
+    public string FullAddress => $"{Address}, {City}, {State}";
 }
