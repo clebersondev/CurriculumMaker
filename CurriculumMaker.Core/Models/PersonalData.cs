3a01d31a936fd14f8043e2fdea7d14b46ec71187
Cleberson Pinheiro <clebersonpinheiro.net@gmail.com>
2026-05-19 10:58:54 -0300

    refactor: remove PersonalData id property

--- CurriculumMaker.Core/Models/PersonalData.cs
+++ CurriculumMaker.Core/Models/PersonalData.cs
@@ -24,7 +24,6 @@ public sealed class PersonalData
         State = state;
     }
 
-    public int Id { get; set; }
     [Required] public string Name { get; set; } = string.Empty;
     [Required] public string Surname { get; set; } = string.Empty;
     [EmailAddress] public string Email { get; set; } = string.Empty;
