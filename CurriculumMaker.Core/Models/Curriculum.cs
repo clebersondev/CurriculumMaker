71aca870efe0336adfd7a9b4dadd900852686870
Cleberson Pinheiro <clebersonpinheiro.net@gmail.com>
2026-05-19 10:46:32 -0300

    refactor: add properties to Curriculum model

--- CurriculumMaker.Core/Models/Curriculum.cs
+++ CurriculumMaker.Core/Models/Curriculum.cs
@@ -5,5 +5,15 @@ namespace CurriculumMaker.Core.Models;
 
 public sealed class Curriculum : IAggregateRoot
 {
-    [Required] public PersonalData? PersonalData { get; set; }
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Model name is required.")]
+    [StringLength(50)]
+    public required string ModelName { get; set; } = string.Empty;
+
+    [Required] public required PersonalData PersonalData { get; set; }
+    public List<WorkExperience> WorkExperiences { get; set; } = [];
+    public List<AcademicBackground> AcademicBackgrounds { get; set; } = [];
+    public List<Language> Languages { get; set; } = [];
+    public List<Skill> Skills { get; set; } = [];
 }
